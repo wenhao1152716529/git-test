f29e7853df763e27fd34f271e0aa4d89c03670e1
abc <abc@163.com>
2018-10-15 19:07:09 +0800

    添加工程

---
+++ git-test-project/src/com/itheima/firsttest.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+public class firsttest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+
+
+}
